4712ecce16735296e6b45e34000a4d73774b7814
Ross Khanas <rtkhanas@users.noreply.github.com>
2016-11-28 20:32:36 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ const runFormatSeparatedNumberTest = (inputCountryCode, inputNumber, expectedRes
   assert.equal(formattedNumber, expectedResult);
 };
 
-describe('Format Ukrainian number', function() {
+describe('format Ukrainian number', function() {
   const expectedResult = '+380 (50) 123-45-67';
   it('merged ', () => runFormatMergedNumberTest('+380501234567',expectedResult));
   it('separated', () => runFormatSeparatedNumberTest('+380', '501234567', expectedResult));
