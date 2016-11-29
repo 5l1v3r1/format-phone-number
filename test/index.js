3f66600c15572b394d4412732ca7757905810033
Ross Khanas <rtkhanas@users.noreply.github.com>
2016-11-29 10:13:22 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -11,8 +11,14 @@ const runFormatSeparatedNumberTest = (inputCountryCode, inputNumber, expectedRes
   assert.equal(formattedNumber, expectedResult);
 };
 
+describe('format Korean number', function() {
+  const expectedResult = '+82 10 1234-5678';
+  it('merged ', () => runFormatMergedNumberTest('+821012345678', expectedResult));
+  it('separated', () => runFormatSeparatedNumberTest('+82', '1012345678', expectedResult));
+});
+
 describe('format Ukrainian number', function() {
   const expectedResult = '+380 (50) 123-45-67';
-  it('merged ', () => runFormatMergedNumberTest('+380501234567',expectedResult));
+  it('merged ', () => runFormatMergedNumberTest('+380501234567', expectedResult));
   it('separated', () => runFormatSeparatedNumberTest('+380', '501234567', expectedResult));
 });
